d7e391baa2d8e04496346634472c59a60472a677
iamjunior <myjuniorprog@gmail.com>
2019-10-24 20:24:55 +0700

    User Controller store dan update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -56,7 +56,6 @@ class UserController extends Controller
         $user->address  = $request->get('address');
         $user->phone    = $request->get('phone');
         $user->status   = $request->get('status');
-
         if($request->file('avatar')){
             if($user->avatar && file_exists(storage_path('app/public/' . $user->avatar)))
             {
