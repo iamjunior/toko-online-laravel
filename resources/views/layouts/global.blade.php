60968748e33ed44ffd101bd1f9218e563570483b
iamjunior <myjuniorprog@gmail.com>
2019-10-28 13:48:24 +0700

    Modul Book Finish

--- resources/views/layouts/global.blade.php
+++ resources/views/layouts/global.blade.php
@@ -80,6 +80,12 @@
                         <span class="oi oi-tag"></span> Manage Categories
                     </a>
                 </li>
+
+                <li>
+                    <a href="{{route('books.index')}}">
+                        <span class="oi oi-book"></span>Manage books
+                    </a>
+                </li>
                 
                 <div class="d-block d-md-none">
                     <div class="dropdown-divider"></div>
